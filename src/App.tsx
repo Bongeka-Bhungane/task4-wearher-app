3cd9f7d522b90f2aab2626283c22d4ebad391711
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-09-25 09:22:16 +0200

    made layout grids of how my content will be structured using divs

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div className="app">
+    <div className='side-bar'>side</div>
+    <div className='content'>
+      <div className='search'>search</div>
+      <div className='city'>city</div>
+      <div className='daily-forecast'>daily</div>
+      <div className='air-conditions'>air conditions</div>
+    </div>
+    <div className='forecast'>forecast</div>
+    </div>
   )
 }
 
