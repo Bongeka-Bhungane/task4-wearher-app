dbfd0b317621335ecaa3d2962053af2b1e8abdde
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-09-25 09:52:23 +0200

    added the searchbar and text components from my previous app i will reuse them, set up the json server

---
+++ src/components/text.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type props = {
+  variant?: string;
+  children: React.ReactNode;
+  className?: string;
+  style?: React.CSSProperties;
+};
+const Text: React.FC<props> = ({ variant, children, style, className }) => {
+  if (variant === "h2") return <h2 style={style} className={className}>{children}</h2>;
+  if (variant === "p") return <p style={style}>{children}</p>;
+  if (variant === "a") return <a style={style}>{children}</a>;
+    return (
+    <div style={style}>{children}</div>
+);
+};
+
+export default Text;
